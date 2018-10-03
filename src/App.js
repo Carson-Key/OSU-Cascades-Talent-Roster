98f8bc880f01635b1f59fe84b45041ca0f98d064
Colin Suckow <csuckow99@gmail.com>
2018-10-03 13:40:27 -0700

    Implemented content columns and began the filter box

--- src/App.js
+++ src/App.js
@@ -10,9 +10,51 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div class="content-container">
+          <div class="column-container">
+
+            <div class="filter-column">
+              <div class="filter-container">
+              <h3 class="filter-title"> Filter </h3>
+                <div class="filter-option">
+                  <h4 class="filter-item-title"> Option 1 </h4>
+                  <select class="filter-item-select">
+                    <option> value 1 </option>
+                    <option> value 2 </option>
+                    <option> value 3 </option>
+                  </select>
+                </div>
+
+                <div class="filter-option">
+                  <h4 class="filter-item-title"> Option 2 </h4>
+                  <select class="filter-item-select">
+                    <option> value 1 </option>
+                    <option> value 2 </option>
+                    <option> value 3 </option>
+                  </select>
+                </div>
+
+                <div class="filter-option">
+                  <h4 class="filter-item-title"> Option 3 </h4>
+                  <select class="filter-item-select">
+                    <option> value 1 </option>
+                    <option> value 2 </option>
+                    <option> value 3 </option>
+                  </select>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="content-column">
+              <p>
+              Content
+              </p>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
